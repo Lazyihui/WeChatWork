81be374ebfc394e6da0e4979d6488d8bfcdc36b6
Lazyihui <chengbaozi6@qq.com>
2025-03-16 16:27:28 +0800

    <feature>add: 比较两个数字

---
+++ pages/index/index.js
@@ -0,0 +1,47 @@
+Page({
+  data: {
+    num1: null, // 第一个数
+    num2: null, // 第二个数
+    result: '', // 比较结果
+  },
+
+  // 监听第一个输入框的输入事件
+  onInputNum1(e) {
+    this.setData({
+      num1: parseFloat(e.detail.value), // 将输入的值转换为数字
+    });
+  },
+
+  // 监听第二个输入框的输入事件
+  onInputNum2(e) {
+    this.setData({
+      num2: parseFloat(e.detail.value), // 将输入的值转换为数字
+    });
+  },
+
+  // 比较两个数的大小
+  compare: function (e) {
+    const { num1, num2 } = this.data;
+   
+    if (num1 === null || num2 === null) {
+      this.setData({
+        result: '请输入两个数',
+      });
+      return;
+    }
+
+    if (num1 > num2) {
+      this.setData({
+        result: `${num1} 大于 ${num2}`,
+      });
+    } else if (num1 < num2) {
+      this.setData({
+        result: `${num1} 小于 ${num2}`,
+      });
+    } else {
+      this.setData({
+        result: `${num1} 等于 ${num2}`,
+      });
+    }
+  },
+});
